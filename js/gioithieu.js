fb3f4d008d5e5c1030db3729bf67ee3228a340bd
hatake1608 <dominh162002@gmail.com>
2022-02-27 21:45:42 +0700

    fix bug

--- js/gioithieu.js
+++ js/gioithieu.js
@@ -1,16 +1,14 @@
-const qualified = document.querySelectorAll(".qualified-item-inner");
-const qualifiedContents = document.querySelectorAll(".qualified-content");
-for(let i = 0; i<qualified.length;i++){
-    qualified[i].onclick = () => {
-        if(qualifiedContents[i].className == "qualified-content"){
-            qualifiedContents[i].classList.add('active')
+$(document).ready(function() {
+    $('.qualified-content').hide();
+    $('.qualified-item').click(function () {
+        var index = $(this).index();
+        var qualifiedContent = $('.qualified-content').eq(index)
+        if (qualifiedContent.hasClass('active')){
+            qualifiedContent.removeClass('active');
+            qualifiedContent.hide(500);
         }else{
-            qualifiedContents[i].classList.remove('active')
+            qualifiedContent.addClass('active');
+            qualifiedContent.show(500);
         }
-        for(let j = 0; j < qualified.length; j++){
-            if(j!=i){
-                qualifiedContents[j].classList.remove('active')
-            }
-        }
-    }
-}
+    })
+})
