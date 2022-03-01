18932443638b9ed0cd0a2e191d341970eccfa539
hatake1608 <dominh162002@gmail.com>
2022-03-01 17:25:16 +0700

    change name files

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.qualified-content').hide();
     $('.qualified-item').click(function () {
+        $('.qualified-content').hide(500);
         var index = $(this).index();
         var qualifiedContent = $('.qualified-content').eq(index)
         if (qualifiedContent.hasClass('active')){
